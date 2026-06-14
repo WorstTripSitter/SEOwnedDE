973f2a7554c5b7ff79fe9f8f6ce9d3aa03f7bf13
rei-2 <174158077+rei-2@users.noreply.github.com>
2026-06-13 21:06:08 -0400

    fix for latest game update

--- SEOwnedDE/SEOwnedDE/src/App/Hooks/CTFDroppedWeapon.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Hooks/CTFDroppedWeapon.cpp
@@ -4,7 +4,7 @@
 
 MAKE_SIGNATURE(CTFDroppedWeapon_OnDataChanged, "client.dll", "40 55 53 41 57 48 8D 6C 24", 0x0);
 MAKE_SIGNATURE(CTFDroppedWeapon_ClientThink, "client.dll", "48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 83 EC ? 4C 89 7C 24", 0x0);
-MAKE_SIGNATURE(CTFDroppedWeapon_OnInternalDrawModel, "client.dll", "48 89 74 24 ? 57 48 81 EC ? ? ? ? 48 8B F2 48 8B F9 E8 ? ? ? ? 84 C0", 0x0);
+MAKE_SIGNATURE(CTFDroppedWeapon_OnInternalDrawModel, "client.dll", "40 55 56 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B F2", 0x0);
 
 MAKE_HOOK(CTFDroppedWeapon_OnDataChanged, Signatures::CTFDroppedWeapon_OnDataChanged.Get(), void, __fastcall,
 	void* ecx, DataUpdateType_t updateType)
